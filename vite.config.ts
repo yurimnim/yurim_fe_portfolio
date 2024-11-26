1c9b4c36706fd21c9086f627452168d2829cd8ff
Yurim Lee <u007x2@gmail.com>
2024-11-27 01:02:09 +0900

    fixed typo on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/yurim-fe-portfolio/',  
+  base: '/yurim_fe_portfolio/',  
   server: {
     port: 3000,
     open: true, 
