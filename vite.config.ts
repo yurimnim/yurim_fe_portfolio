d83198192a21141ec3caf27c77a3a839af740d43
Yurim Lee <u007x2@gmail.com>
2024-11-26 23:50:23 +0900

    fixed deploy workflow and vite config and package json 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +14,11 @@ export default defineConfig({
   ],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
   base: '/yurim_fe_portfolio/',
   publicDir: 'public',
